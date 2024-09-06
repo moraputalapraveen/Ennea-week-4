e9237e2a54a663a6179c59f460f47ce8927ae1c5
moraputalapraveen <moraputalapraveen@gmail.com>
2024-09-06 23:18:46 +0530

    hashrouter changed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import {QueryClientProvider,QueryClient} from '@tanstack/react-query';
 
@@ -15,12 +15,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
-    <BrowserRouter>
+    <HashRouter>
     <QueryClientProvider client={client1}>
      
     <App />
     </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
    
   </React.StrictMode>
 );
